7768feba5298228d1d286bd014b44d47a8ffeee7
cobit <cobit@coBit-ubuntu>
2017-03-23 08:43:12 -0400

    Writing IP addresses to file with the same name as input keyword.

--- NetworkProject/src/Run.java
+++ NetworkProject/src/Run.java
@@ -1,9 +1,13 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 import org.apache.commons.net.whois.WhoisClient;
@@ -18,7 +22,7 @@ public class Run {
 		FileInputStream inputStream = new FileInputStream(new File(dir));
 		Scanner reader = new Scanner(inputStream, "UTF-8");		
 
-        String keyword = "Stony";
+        String keyword = "irvine";
         
         ArrayList<String> list = new ArrayList<>();
         int index = 0;
@@ -28,7 +32,7 @@ public class Run {
             String[] tokens = line.split(",");
             
             // if found 
-            if(tokens[2].contains(keyword)){
+            if(tokens[2].toLowerCase().contains(keyword.toLowerCase())){
             	String temp = tokens[2];
             	temp = temp.substring(1, temp.length()-1);
             	String[] temp_tokens = temp.split(" ");
@@ -41,32 +45,42 @@ public class Run {
         }
 		
         System.out.println("Found AS list for keyword: " + keyword + "\n" + list);
-        //String command = "whois -h whois.radb.net -- \'-i origin AS5693\' | grep -Eo \"([0-9.]+){4}/[0-9]+\"";
+        String command = "whois -h whois.radb.net -- \'-i origin "+ list.get(0)+"\' | grep -Eo \"([0-9.]+){4}/[0-9]+\"";
+        System.out.println(command + "\n");
         
+        String outFile = keyword+".txt";
+		PrintWriter writer = new PrintWriter(outFile, "UTF-8");
         
-        String command2 = "-i origin AS299";
-    	StringBuilder result = new StringBuilder("");    	
-		WhoisClient whois = new WhoisClient();
-		try {
-			whois.connect("whois.radb.net");
-			String whoisData = whois.query(command2);
-			result.append(whoisData);
-			whois.disconnect();
+		for(int j=0; j<list.size(); j++){ // iterating on list of AS numbers 
+        
+			// whois queriying
+	        String command2 = "-i origin " + list.get(j);
+	    	StringBuilder result = new StringBuilder("");    	
+			WhoisClient whois = new WhoisClient();
+			try {
+				whois.connect("whois.radb.net");
+				String whoisData = whois.query(command2);
+				result.append(whoisData);
+				whois.disconnect();
+	
+			}  catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			
+			// regex spliting router IP addresses 
+			String[] lines = result.toString().split("\n");
+			for(int i=0; i<lines.length ; i++){
+				if(lines[i].contains("route:")){
+					System.out.println(lines[i].substring(7).trim());
+					writer.println(lines[i].substring(7).trim());
+				}				
+			}
+			
+		}
+		
+		writer.close();
 
-		}  catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
-		
-		String[] lines = result.toString().split("\n");
-		for(int i=0; i<lines.length ; i++){
-			if(lines[i].contains("route:"))
-				System.out.println(lines[i]);
-		}
-		
-		
-		
 		
 	}
 
