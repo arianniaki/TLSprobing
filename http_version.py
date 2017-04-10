bfe35d8cf90c421f1a083e71b29d88e2e1ec85df
cobit <cobit@coBit-ubuntu>
2017-04-10 14:03:10 -0400

    http_version.py is updated. read from _servers and for each http version

--- http_version.py
+++ http_version.py
@@ -1,12 +1,21 @@
 # http://stackoverflow.com/questions/1210883/determine-supported-http-version-by-the-web-server
 from netaddr import IPNetwork
 import subprocess
+import sys
 
+http_vers = ['--http1.0', '--http1.1', '--http2']
 
+file_to_read = sys.argv[1]
+F = open(file_to_read,"r")
+list_of_servers = F.readlines()
 
-for ip in IPNetwork('130.245.0.0/16'):
-	print '%s' % ip
-	p = subprocess.Popen(["curl",'--http1.1','--head' ,str(ip),'-m','1'], stdout=subprocess.PIPE)
-	out, err = p.communicate()
-#print out
-	print(out)
+file_to_read_name, file_to_read_format = file_to_read.split('.')
+for http in http_vers:
+	print('==========='+ http + ' (START) =====================')
+	for server in list_of_servers:
+		url,servername,subnet = server.split(',')
+		print '%s' % url
+		p = subprocess.Popen(["curl",http,'--head' ,str(url),'-m','1'], stdout=subprocess.PIPE)
+		out, err = p.communicate()
+		print(out)
+	print('==========='+ http + ' (END) =====================')
