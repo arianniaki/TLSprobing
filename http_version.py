9a31e0e9678928d6e8fd882cc081fe5375a88b83
cobit <cobit@coBit-ubuntu>
2017-04-30 05:25:31 -0400

    http_version is fixed for http2 and writing into file.

--- http_version.py
+++ http_version.py
@@ -10,12 +10,22 @@ F = open(file_to_read,"r")
 list_of_servers = F.readlines()
 
 file_to_read_name, file_to_read_format = file_to_read.split('.')
-for http in http_vers:
-	print('==========='+ http + ' (START) =====================')
-	for server in list_of_servers:
-		url,servername,subnet = server.split(',')
+
+f= open("output.txt","w+")
+
+for server in list_of_servers:
+	url,servername,subnet = server.split(',')
+	f.write("Url: %s \n" % url)
+	for http in http_vers:		
 		print '%s' % url
-		p = subprocess.Popen(["curl",http,'--head' ,str(url),'-m','1'], stdout=subprocess.PIPE)
+		p = subprocess.Popen(["curl",http,'--head', '-k' ,str(url),'-m','2'], stdout=subprocess.PIPE)
 		out, err = p.communicate()
+		print('=====ss======')
 		print(out)
-	print('==========='+ http + ' (END) =====================')
+		print('=====ee======')
+		f.write("\t HTTP versions : %s \n" % http)	
+		if out:
+			out2 = out.splitlines()		
+			f.write("\t\t %s \n " % out2[0])
+		
+f.close()	
