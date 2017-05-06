31f664384101408a859dda5c389ebc18b4d8a4b1
cobit <cobit@coBit-ubuntu>
2017-05-06 14:30:23 -0400

    http-version json writing added!

--- http_version.py
+++ http_version.py
@@ -2,6 +2,7 @@
 from netaddr import IPNetwork
 import subprocess
 import sys
+import json
 
 http_vers = ['--http1.0', '--http1.1', '--http2']
 
@@ -11,11 +12,18 @@ list_of_servers = F.readlines()
 
 file_to_read_name, file_to_read_format = file_to_read.split('.')
 
-f= open("output.txt","w+")
+#f= open("output.txt","w+")
+
+response_json = {}
+children = []
 
 for server in list_of_servers:
 	url,servername,subnet = server.split(',')
-	f.write("Url: %s \n" % url)
+	data = {}
+	data['university'] = file_to_read_name
+	data['url'] = url
+	have_response = False
+	#f.write("Url: %s \n" % url)
 	for http in http_vers:		
 		print '%s' % url
 		p = subprocess.Popen(["curl",http,'--head', '-k' ,str(url),'-m','2'], stdout=subprocess.PIPE)
@@ -23,9 +31,18 @@ for server in list_of_servers:
 		print('=====ss======')
 		print(out)
 		print('=====ee======')
-		f.write("\t HTTP versions : %s \n" % http)	
+		#f.write("\t HTTP versions : %s \n" % http)	
 		if out:
-			out2 = out.splitlines()		
-			f.write("\t\t %s \n " % out2[0])
+			out2 = out.splitlines()	
+			data[http] =  out2[0]
+			have_response = True	
+			#f.write("\t\t %s \n " % out2[0])
+	if have_response:
+		children.append(data)
+response_json["servers"] = children
+print json.dumps(response_json,indent=2)
+with open(file_to_read_name + '_openssl.json', 'a') as outfile:
+	json.dump(response_json, outfile)
+
 		
-f.close()	
+#f.close()	
