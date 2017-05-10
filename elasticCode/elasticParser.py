ed977655227dad7dbed983a13ed1beefc53787bd
Arian Niaki <arianniaki@gmail.com>
2017-05-09 20:06:46 -0400

    http version bank added

--- elasticCode/elasticParser.py
+++ elasticCode/elasticParser.py
@@ -5,10 +5,10 @@ from glob import glob
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 outlist = []
-for infile in glob('json_bank_openssl/*'):
+for infile in glob('json_bank_http/*'):
     with open(infile, 'r') as of:
     	print(infile)
-        f = json.loads(of.read())
+        f = json.loads(of.read().replace('\r','\\r').replace('\n','\\n'),strict=False)
         outlist += f['servers']
         #outlist += f['servers']
 print len(outlist)
@@ -17,5 +17,5 @@ print len(outlist)
 i = 0
 for item in outlist:
     i=i+1
-    es.index(index='openssls', doc_type='openssl', id=i, body=json.loads(item))
+    es.index(index='http', doc_type='http', id=i, body=json.loads(item.replace('\r','\\r').replace('\n','\\n'),strict=False))
     
